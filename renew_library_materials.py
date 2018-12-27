17ae20f3b5d7a12c457e842a1470b62d6ed09a77
Shawn Cresante <shawn@cresante.com>
2018-12-27 17:04:25 -0500

    latest changes

--- renew_library_materials.py
+++ renew_library_materials.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from time import sleep
-from sys import exit, argv
-import re, datetime
-import os
+import re
+import datetime
+# import os
+# from time import sleep
+from sys import argv
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -12,6 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 MOST_DAYS_OUT = 3
+ACT = True
 
 url = 'http://lwpl.sirsi.net'
 
@@ -20,20 +22,18 @@ if (pd).is_file():
     with open(pd) as credfile:
         user, pin = credfile.readline().strip().split(' ')
 else:
-        user, pin = argv[1], argv[2]
-
+    user, pin = argv[1], argv[2]
     # user = input("Enter library card #: ")
     # pin = input("Enter PIN: ")
 
-
 options = Options()
 # options.set_headless(headless=True)
-browser = webdriver.Firefox(firefox_options = options)
+browser = webdriver.Chrome(options=options)
 browser.get(url)
 
 # wait for item to appear, then click it
 loginnow = WebDriverWait(browser, 10).until(\
-        ec.visibility_of_element_located((By.CLASS_NAME, "login_button")));
+        ec.visibility_of_element_located((By.CLASS_NAME, "login_button")))
 # print("finished waiting")
 uid = browser.find_element_by_id('user_id')
 uid.send_keys(user)
@@ -43,18 +43,18 @@ password.send_keys(pin)
 loginnow.click()
 # print("sleeping")
 renew_link = WebDriverWait(browser, 10).until(\
-        ec.visibility_of_element_located((By.LINK_TEXT, "Renew My Materials")));
+        ec.visibility_of_element_located((By.LINK_TEXT, "Renew My Materials")))
 # renew_link = browser.find_element_by_link_text('Renew My Materials')
 renew_link.click()
 
-due_dates = [ _.text for _ in browser.find_elements_by_xpath("//td[@align='left']")]
+due_dates = [_.text for _ in browser.find_elements_by_xpath("//td[@align='left']")]
 print("Previous due dates:")
 print(*due_dates, sep='\n')
 
 expire_soon = False
 
 for date in due_dates:
-    m = re.match('Due: (\d+)/(\d+)/(\d+),(\d+):(\d+)', date)
+    m = re.match(r'Due: (\d+)/(\d+)/(\d+),(\d+):(\d+)', date)
     if m:
         mo, day, yr, hr, mi = [int(_) for _ in m.groups()]
         due_on = datetime.datetime(yr, mo, day, hr, mi)
@@ -63,15 +63,17 @@ for date in due_dates:
             expire_soon = True
             print(f"Found a due date that is within {MOST_DAYS_OUT} days!: {m}")
 
-if expire_soon == True:
+if expire_soon:
     browser.find_element_by_id("renew_all").click()
-    browser.find_element_by_xpath("//input[@value='Renew Selected Items']").click()
+    if ACT:
+        browser.find_element_by_xpath("//input[@value='Renew Selected Items']").click()
     due_dates = WebDriverWait(browser, 10).until(\
-            ec.visibility_of_all_elements_located((By.XPATH,"//td[@align='left']")))
-    due_dates = [ _.text for _ in due_dates]
+            ec.visibility_of_all_elements_located((By.XPATH, "//td[@align='left']")))
+    due_dates = [_.text for _ in due_dates]
     print("New due dates:")
     print(*due_dates, sep='\n')
 else:
     print(f"No items due within the {MOST_DAYS_OUT} day limit")
 
-browser.close()
+if ACT:
+    browser.close()
