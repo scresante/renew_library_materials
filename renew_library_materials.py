9bb7f2e04cfb14e2ec8399ed630ad88478607457
Shawn Cresante <shawn@cresante.com>
2018-12-27 17:12:14 -0500

    cleanup

--- renew_library_materials.py
+++ renew_library_materials.py
@@ -2,8 +2,7 @@
 
 import re
 import datetime
-# import os
-# from time import sleep
+from time import sleep
 from sys import argv
 from pathlib import Path
 from selenium import webdriver
@@ -23,28 +22,23 @@ if (pd).is_file():
         user, pin = credfile.readline().strip().split(' ')
 else:
     user, pin = argv[1], argv[2]
-    # user = input("Enter library card #: ")
-    # pin = input("Enter PIN: ")
 
 options = Options()
-# options.set_headless(headless=True)
+if ACT:
+    options.set_headless(headless=True)
 browser = webdriver.Chrome(options=options)
 browser.get(url)
 
 # wait for item to appear, then click it
 loginnow = WebDriverWait(browser, 10).until(\
         ec.visibility_of_element_located((By.CLASS_NAME, "login_button")))
-# print("finished waiting")
 uid = browser.find_element_by_id('user_id')
 uid.send_keys(user)
 password = browser.find_element_by_id('password')
 password.send_keys(pin)
-# loginnow = browser.find_element_by_class('login_button')
 loginnow.click()
-# print("sleeping")
 renew_link = WebDriverWait(browser, 10).until(\
         ec.visibility_of_element_located((By.LINK_TEXT, "Renew My Materials")))
-# renew_link = browser.find_element_by_link_text('Renew My Materials')
 renew_link.click()
 
 due_dates = [_.text for _ in browser.find_elements_by_xpath("//td[@align='left']")]
@@ -67,7 +61,8 @@ if expire_soon:
     browser.find_element_by_id("renew_all").click()
     if ACT:
         browser.find_element_by_xpath("//input[@value='Renew Selected Items']").click()
-    due_dates = WebDriverWait(browser, 10).until(\
+    sleep(2)
+    due_dates = WebDriverWait(browser, 10, poll_frequency=2).until(\
             ec.visibility_of_all_elements_located((By.XPATH, "//td[@align='left']")))
     due_dates = [_.text for _ in due_dates]
     print("New due dates:")
